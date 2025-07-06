b883019e21b78305d62cfb38093c7ae591c36979
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-06 13:03:43 +0300

    Init

---
+++ src/test/java/io/github/mitohondriyaa/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.mitohondriyaa.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
