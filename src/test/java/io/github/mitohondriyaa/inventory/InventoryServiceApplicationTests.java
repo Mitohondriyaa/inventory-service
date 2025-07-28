d1a81665a95397642e05d673c3f629bf250dc7bc
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 21:58:24 +0300

    Add inventory-reserved schema

--- src/test/java/io/github/mitohondriyaa/inventory/InventoryServiceApplicationTests.java
+++ src/test/java/io/github/mitohondriyaa/inventory/InventoryServiceApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
