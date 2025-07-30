ff2661b75708e187f639d72772c9f9f067913fd0
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-30 16:40:44 +0300

    Add service for redis

---
+++ src/main/java/io/github/mitohondriyaa/inventory/service/RedisService.java
@@ -0,0 +1,20 @@
+package io.github.mitohondriyaa.inventory.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final StringRedisTemplate stringRedisTemplate;
+    @Value("${message-id.ttl.minutes}")
+    private Integer messageIdTtlMinutes;
+
+    public Boolean setValue(String key) {
+        return stringRedisTemplate.opsForValue().setIfAbsent(key, "", Duration.ofMinutes(messageIdTtlMinutes));
+    }
+}
