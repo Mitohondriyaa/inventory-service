7a21596fd123dba0decb746215150977a33ae01c
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-30 17:52:32 +0300

    Add authentication and authorization error handling

--- src/main/java/io/github/mitohondriyaa/inventory/config/SecurityConfig.java
+++ src/main/java/io/github/mitohondriyaa/inventory/config/SecurityConfig.java
@@ -3,14 +3,50 @@ package io.github.mitohondriyaa.inventory.config;
 import io.github.mitohondriyaa.inventory.converter.KeycloakRealmRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write(
+                """
+                    {
+                        "code": "UNAUTHORIZED",
+                        "message": "User not authenticated"
+                    }
+                    """
+            );
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                        "code": "FORBIDDEN",
+                        "message": "User not authorized"
+                    }
+                """);
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+        HttpSecurity http,
+        AuthenticationEntryPoint entryPoint,
+        AccessDeniedHandler handler
+    ) throws Exception {
         return http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(
                     "/swagger-ui/**",
@@ -21,6 +57,8 @@ public class SecurityConfig {
                 .anyRequest()
                 .hasRole("INVENTORY_MANAGER"))
             .oauth2ResourceServer(oauth2 -> oauth2
+                .authenticationEntryPoint(entryPoint)
+                .accessDeniedHandler(handler)
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakRealmRoleConverter())))
             .build();
     }
